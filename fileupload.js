462fbb8be48648b66eca457213f58ba69f9e8891
Franck Mugisho <fbmugisho@gmail.com>
2022-11-09 12:34:22 -0500

    adding speech recognition logic

--- fileupload.js
+++ fileupload.js
@@ -101,3 +101,39 @@ window.onload = function() {
   }
 
 }
+
+/* runSpeechRecognition function */
+function runSpeechRecognition() {
+  var transcript;
+  // get output div reference
+  var output = document.getElementById("output");
+  // get action element reference
+  var action = document.getElementById("action");
+  // new speech recognition object
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+
+  // This runs when the speech recognition service starts
+  recognition.onstart = function() {
+      // action.innerHTML = "<small>listening, please speak...</small>";
+  };
+
+  recognition.onspeechend = function() {
+      // action.innerHTML = "<small>stopped listening, hope you are done...</small>";
+      recognition.stop();
+  }
+
+  // This runs when the speech recognition service returns result
+  recognition.onresult = function(event) {
+      var q = document.getElementById("q"); // addition
+      transcript = event.results[0][0].transcript;
+      var confidence = event.results[0][0].confidence;
+      // output.innerHTML = "<b>Text:</b> " + transcript + "<br/> <b>Confidence:</b> " + confidence * 100 + "%";
+      q.value = transcript;
+      output.classList.remove("hide");
+  };
+
+  // start recognition
+  recognition.start();
+  return transcript
+}
